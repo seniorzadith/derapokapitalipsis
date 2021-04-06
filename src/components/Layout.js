93e4ab8c5917f6e970ef6bcc6f0e917a5ccc176d
seniorzadith <you@example.com>
2021-04-06 13:28:20 -0400

    upload un poco actualizado

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,13 +4,16 @@ import Footer from "./Footer"
 import { Helmet } from "react-helmet";
 import urwdin from '../fonts/URWDIN-Bold.woff'
 import brolga from '../fonts/Brolga.woff'
+import dooms from '../fonts/DoomsDay.woff'
 import "./layout.css"
 
 const Layout = ({ children }) => {
   return (
     <main>
       <Helmet>
-        <link rel="preload" href={urwdin, brolga} as="fonts/woff" crossOrigin="anonymous" type="font/woff2" />
+        <link rel="preload" href={urwdin} as="fonts/woff" crossOrigin="anonymous" type="font/woff2" />
+        <link rel="preload" href={brolga} as="fonts/woff" crossOrigin="anonymous" type="font/woff2" />
+        <link rel="preload" href={dooms} as="fonts/woff" crossOrigin="anonymous" type="font/woff2" />
       </Helmet>
       <Navbar />
       {children}
